92d0dfd822c1e2cf907bb013c90bc82dc0e792d5
kabushPasha <kabushpavel@gmail.com>
2026-06-08 15:46:45 +0200

    updates

--- client/src/classes/LEVELS/3D_WalkerLevel/3D_WalkerLevel.tsx
+++ client/src/classes/LEVELS/3D_WalkerLevel/3D_WalkerLevel.tsx
@@ -4,6 +4,14 @@ import { Walker3D_Player } from "./classes/Player3DWalker";
 import { useGLTF } from "@react-three/drei";
 import { useEffect } from "react";
 import * as THREE from "three/webgpu";
+import { CameraUniformsProvider } from "../../PostProcessing/cameraUniformsContext";
+import { WebGPUPostProcessingProvider } from "../../PostProcessing/PostProcessingContext";
+import { PP_FogPass } from "../../PostProcessing/Effects/PP_FogPass";
+import { PP_Sharpen } from "../../PostProcessing/Effects/PP_Sharpen";
+import { PP_DoF, PP_Scanline, PP_Vignette } from "../../PostProcessing/Effects/PP_Dof";
+import { PP_ColorGrading } from "../../PostProcessing/Effects/PP_ColorGrading";
+import { PP_LUT } from "../../PostProcessing/Effects/PP_3DLUTPass";
+import { PP_PalDither } from "../../PostProcessing/Effects/PP_PalDither";
 
 
 
@@ -13,11 +21,27 @@ export function Walker3DLevel() {
 
 
     return <>
+        <CameraUniformsProvider>
+            <WebGPUPostProcessingProvider >
+
+                <PP_FogPass density={0.5 * 0.01} heightFalloff={0.01} />
+                <PP_Sharpen />
+                <PP_DoF />
+                <PP_ColorGrading />
+                <PP_LUT />
+                <PP_Vignette />
+                <PP_PalDither />
+                <PP_Scanline />
+
+            </WebGPUPostProcessingProvider>
+        </CameraUniformsProvider>
+
+
         <Pixelated resolution={512} enabled={true} />
 
-        {0 &&<>
+        {1 && <>
             <ambientLight intensity={1} />
-            <directionalLight position={[5, 10, 5]} intensity={2} />
+            <directionalLight position={[5, 10, 5]} intensity={0} />
         </>}
 
         <Physics gravity={[0, -9.81, 0]}>
@@ -44,8 +68,13 @@ export function Ground() {
 
 export function Level() {
     const { scene } = useGLTF("models/Level/test2.glb");
+    const { scene: scene2 } = useGLTF("models/Level/test3.glb");
 
+    console.log("Loaded Scene", scene2);
+    /*
     useEffect(() => {
+        console.log("scene",scene);
+
         const material = new THREE.MeshBasicMaterial({
             vertexColors: true,
             side: THREE.DoubleSide,
@@ -63,10 +92,16 @@ export function Level() {
             material.dispose();
         };
     }, [scene]);
+    */
 
     return (
-        <RigidBody type="fixed" colliders="trimesh">
-            <primitive object={scene} />
-        </RigidBody>
+        <>
+            <RigidBody type="fixed" colliders="trimesh">
+                <primitive object={scene} />
+            </RigidBody>
+
+            {0 && <primitive object={scene2} />}
+
+        </>
     );
 }
